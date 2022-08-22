2c710dfc515a51d58a926deb7a75ed5aa73618a4
alexmitroff <alexmitroff@gmail.com>
2022-08-23 00:47:26 +0300

    Add basic movement

--- Assets/Scripts/Behaviour/BehaviourArrow.cs
+++ Assets/Scripts/Behaviour/BehaviourArrow.cs
@@ -6,23 +6,52 @@ namespace PrawnEntertainment.Behaviour
 {
     public class BehaviourArrow : MonoBehaviour
     {
-        public Transform OnLeft;
-        public Transform OnForward;
-        public Transform OnRight;
+        public Transform LeftMove;
+        public Transform ForwardMove;
+        public Transform RightMove;
+
+        private Transform _SelfTranform;
+
+        void Awake()
+        {
+            _SelfTranform = transform;
+        }
 
         public void SetForward(Transform transform)
         {
-            OnForward = transform;
+            ForwardMove = transform;
         }
 
         public void SetLeft(Transform transform)
         {
-            OnLeft = transform;
+            LeftMove = transform;
         }
 
         public void SetRight(Transform transform)
         {
-            OnRight = transform;
+            RightMove = transform;
+        }
+
+        public void OnLeft()
+        {
+            Transform Target = LeftMove;
+            LeftMove = null;
+            _SelfTranform.position = Target.position;
+            _SelfTranform.rotation = Target.rotation;
+        }
+        public void OnForward()
+        {
+            Transform Target = ForwardMove;
+            ForwardMove = null;
+            _SelfTranform.position = Target.position;
+            _SelfTranform.rotation = Target.rotation;
+        }
+        public void OnRight()
+        {
+            Transform Target = RightMove;
+            RightMove = null;
+            _SelfTranform.position = Target.position;
+            _SelfTranform.rotation = Target.rotation;
         }
     }
 }
