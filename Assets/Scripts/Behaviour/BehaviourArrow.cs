f376788acf50c4dc2d74bca28a62db0d6417245b
alexmitroff <alexmitroff@gmail.com>
2022-08-28 02:38:04 +0300

    Figuring out a movements

--- Assets/Scripts/Behaviour/BehaviourArrow.cs
+++ Assets/Scripts/Behaviour/BehaviourArrow.cs
@@ -6,9 +6,15 @@ namespace PrawnEntertainment.Behaviour
 {
     public class BehaviourArrow : MonoBehaviour
     {
+        [Header("Move variants")]
         public Transform LeftMove;
         public Transform ForwardMove;
         public Transform RightMove;
+
+        [Header("Replacements")]
+        public GameObject PrefabTurnLeft;
+        public GameObject PrefabTurnRight;
+        public GameObject PrefabStright;
 
         private Transform _SelfTranform;
 
@@ -34,24 +40,69 @@ namespace PrawnEntertainment.Behaviour
 
         public void OnLeft()
         {
-            Transform Target = LeftMove;
-            LeftMove = null;
-            _SelfTranform.position = Target.position;
-            _SelfTranform.rotation = Target.rotation;
+            if (LeftMove == null) return;
+            _SpawnPlaceholder(LeftMove, PrefabTurnLeft);
+            _MakeAMove(LeftMove);
         }
         public void OnForward()
         {
-            Transform Target = ForwardMove;
-            ForwardMove = null;
-            _SelfTranform.position = Target.position;
-            _SelfTranform.rotation = Target.rotation;
+            if (ForwardMove == null) return;
+            _SpawnPlaceholder(ForwardMove, PrefabStright);
+            _MakeAMove(ForwardMove);
         }
         public void OnRight()
         {
-            Transform Target = RightMove;
+            if (RightMove == null) return;
+            _SpawnPlaceholder(RightMove, PrefabTurnRight);
+            _MakeAMove(RightMove);
+        }
+
+        private void _MakeAMove(Transform Move)
+        {
+            bool isUp = Move.position.y > _SelfTranform.position.y+1;
+            bool isDown = Move.position.y < _SelfTranform.position.y-1;
+
+            _CleanMoveVariants();
+
+            Debug.Log($"UP DOWN {isUp} {isDown} { isUp || isDown }");
+
+            _SelfTranform.LookAt(Move);
+            if (isUp) _SelfTranform.rotation = _TurnUp();
+            if (isDown) _SelfTranform.rotation = _TurnDown();
+            _SelfTranform.position = Move.position;
+        }
+
+        private void _SpawnPlaceholder(Transform Move, GameObject PlaceholderPrefab)
+        {
+            Move.gameObject.SetActive(false);
+            Instantiate<GameObject>(PlaceholderPrefab, _SelfTranform.position, _SelfTranform.rotation);
+        }
+
+        private void _CleanMoveVariants()
+        {
+            LeftMove = null;
+            ForwardMove = null;
             RightMove = null;
-            _SelfTranform.position = Target.position;
-            _SelfTranform.rotation = Target.rotation;
+        }
+
+        private Quaternion _TurnRight()
+        {
+            return new Quaternion(0,1,0,90);
+        }
+
+        private Quaternion _TurnLeft()
+        {
+            return new Quaternion(0,1,0,-90);
+        }
+
+        private Quaternion _TurnUp()
+        {
+            return new Quaternion(1,0,0,-45);
+        }
+
+        private Quaternion _TurnDown()
+        {
+            return new Quaternion(1,0,0,45);
         }
     }
 }
